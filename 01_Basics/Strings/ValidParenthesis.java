936ba8b21af0ec0e96a661b85550326cb4ad8ade
milind55555 <hanchatemilind291@gmail.com>
2025-06-15 19:26:24 +0200

    Valid Parenthesis

---
+++ 01_Basics/Strings/ValidParenthesis.java
@@ -0,0 +1,21 @@
+public class ValidParenthesis {
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+     public static boolean isValid(String s) {
+        while(true){
+            if(s.contains("()")){
+                s=s.replace("()","");
+            }
+            else if(s.contains("[]")){
+                s=s.replace("[]","");
+            }
+           else if(s.contains("{}")){
+                s=s.replace("{}","");
+            }else{
+                return s.isEmpty();
+            }
+        }
+    }
+}
